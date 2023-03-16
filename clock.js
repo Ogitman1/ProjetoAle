5d27d91e05e08396ba40b4787ffba13ef95d6011
Matheus Santos <matheussfelipe.santos@gmail.com>
2023-03-15 23:28:17 -0300

    Parte 2
    Parte 2

--- clock.js
+++ clock.js
@@ -5,31 +5,29 @@ function countdown() {
     seconds--;
   
     if (seconds < 0) {
-      clearInterval(intervalId);
       timer.innerHTML = "Time's up!";
       result.innerHTML = `Congratulations! You earned ${points} points`
       
-      
     }
-    console.log(seconds)
   }
-  function countdown() {
+
+  const intervalId = setInterval(countdown, 1000); // update every 1000 milliseconds (1 second)
+
+  function countdown1(){
+    const levell = document.getElementById("ll");
+    level = 1
+    levell.innerHTML =`Level: ${level}`;
+    seconds = 20;
+    points = 0;
+    points += 300;
     const timer = document.getElementById("timer");
     timer.innerHTML = seconds;
     seconds--;
-  
-    if (seconds < 0) {
-      clearInterval(intervalId);
-      timer.innerHTML = "Time's up!";
-      result.innerHTML = `Congratulations! You earned ${points} points`
-      
-      
-    }
-    console.log(seconds)
+    
+    
   }
+
   
-  const intervalId = setInterval(countdown, 1000); // update every 1000 milliseconds (1 second)
-  countdown();
 
   
  
--- progam.js
+++ progam.js
@@ -3,23 +3,28 @@ var min = 1;
 var level = 1;
 
 
-function generateEquation(){//Passo 1: Lógica pra montar o "problema"
+function generateEquation(){
+  //Passo 1: Lógica pra montar a equação
     const x = Math.floor(Math.random() *(max - min+ 1) ) + min;
     const equation = `What is X + ${x} = ${x *2} ?`
     return {x, equation};
 }
-function displayEquation() {//Passo 2: Pegando o document pra mostrar o problema
+function displayEquation() {
+  //Passo 2: Pegando o document pra mostrar o problema
     const { x, equation } = generateEquation();
     document.getElementById("equation").innerHTML = equation;
     document.getElementById("answer").value = "";
     document.getElementById("result").innerHTML = "";
+    document.getElementById("ll").innerHTML = `Level: ${level}`;
     return x;
   }
   let x = displayEquation();//execução pra mostrar a operação
   let points = 300;//valor pré-fixado
 
   function checkanswer(){
-    const answer = parseInt(document.getElementById("answer").value);//transformar o valor do input em int
+    //função que checa se a resposta está certa
+    const answer = parseInt(document.getElementById("answer").value);
+    //transformar o valor do input em int
     const result = document.getElementById("result");
     var newans =  (x*2) - x;
     if(answer=== newans ){//Checa se a resposta está correta
